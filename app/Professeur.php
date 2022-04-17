ca741fca88826ad8e767a494446256fa0e17548f
Rabab Fahssi <fhsyrabab07@gmail.com>
2022-04-17 23:41:15 +0000

    second commit

---
+++ app/Professeur.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+
+class Professeur extends Model
+{
+    public function scopeProfs($query)
+    {
+        if (Auth::user()->role_id == 2)
+        {
+            return $query->where('id_user', '=', Auth::user()->id);
+        }
+        
+    }
+    
+}
